14d203e7207e12eed5e1dbf25c16250b3bec1114
GitHub Action <action@github.com>
2025-08-12 18:20:01 +0000

    Sync from main repo on Tue Aug 12 18:20:01 UTC 2025

--- hello-framework/menu.uvod.js
+++ hello-framework/menu.uvod.js
@@ -18,21 +18,21 @@ export const uvodPages = [
   {
     name: "Rodovi",
     pages: [
-      { name: "Rodovi Ilarić",    path: "/pages/0 Uvod/4_Ilaric" },          
-      { name: "Župe", path: "/pages/KONCEPT/Zupe_K" },
-      { name: "Mjesta", path: "/pages/KONCEPT/Mjesta_K" },
+      { name: "Rodovi Ilarić",    path: "/pages/KONCEPT/Rodovi_K" },          
+      { name: "Župe",             path: "/pages/KONCEPT/Zupe_K" },
+      { name: "Mjesta",           path: "/pages/KONCEPT/Mjesta_K" },
     ]
   },
   {
     name: "Obitelji",
     pages: [
-      { name: "Generacije", path: "/pages/KONCEPT/Generacije_K" },
-      { name: "Obitelji", path: "/pages/KONCEPT/Obitelji_K" },
-      { name: "Brakovi", path: "/pages/KONCEPT/Brakovi_K" },
+      { name: "Generacije",   path: "/pages/KONCEPT/Generacije_K" },
+      { name: "Obitelji",     path: "/pages/KONCEPT/Obitelji_K" },
+      { name: "Brakovi",      path: "/pages/KONCEPT/Brakovi_K" },
       { name: "Povezane obitelji", path: "/pages/KONCEPT/Obitelji_povezane_K" },
       { name: "Događaji - zapisi", path: "/pages/KONCEPT/Zapisi_K" },
-      { name: "Bolesti", path: "/pages/KONCEPT/Bolesti_K" },
-      { name: "Groblja", path: "/pages/KONCEPT/Groblje_K" },
+      { name: "Bolesti",     path: "/pages/KONCEPT/Bolesti_K" },
+      { name: "Groblja",     path: "/pages/KONCEPT/Groblje_K" },
     ]
   },
   {
