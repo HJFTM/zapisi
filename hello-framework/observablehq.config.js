c6d09e405ee42ae8d0560cdb77307bf467a27865
GitHub Action <action@github.com>
2025-11-15 18:51:14 +0000

    Sync from main repo on Sat Nov 15 18:51:14 UTC 2025

--- hello-framework/observablehq.config.js
+++ hello-framework/observablehq.config.js
@@ -132,6 +132,6 @@ export default {
   dynamicPaths,
   head: '<link rel="icon" href="observable.png" type="image/png" sizes="32x32">',
   root: "src",
-  style: "styles.css", 
+  //style: "styles.css", 
   footer: '<small>© 2025 Built by Ivan Jularić</small>'
 };
